5a85aacc086ca277cfb9e29ddccbe5625ed8a6eb
NIKHIL LAXKAR <99583091+nikhillaxkar@users.noreply.github.com>
2024-10-10 23:24:52 +0530

    Update constant.js

--- fortend/src/utils/constant.js
+++ fortend/src/utils/constant.js
@@ -3,3 +3,4 @@ export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
 export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 export const REACT_API="http://localhost:8000";
+export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000';

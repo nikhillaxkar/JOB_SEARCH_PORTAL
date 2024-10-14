166add5e8031d4dcb73fab556d1322522637167b
NIKHIL LAXKAR <99583091+nikhillaxkar@users.noreply.github.com>
2024-10-14 20:33:46 +0530

    Delete fortend directory

--- fortend/src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-export const USER_API_END_POINT="https://job-search-portal-10.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://job-search-portal-10.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-search-portal-10.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-search-portal-10.onrender.com/api/v1/company";
-export const REACT_API="https://job-search-portal-10.onrender.com";
-export const APIUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000';
